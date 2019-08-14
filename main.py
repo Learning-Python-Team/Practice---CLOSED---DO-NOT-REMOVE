a83c895da32ce6181370ab09ad525c606573e245
Alexander Large <alexanderklarge@gmail.com>
2019-08-14 16:04:38 +0100

    Adding my branch

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("Hello GitHub Team")
 print("It should exists at least two branchs\nThe master branch for the stable version and the dev branch for the development version")
+
+print('\nHey everyone! \nAlex here.\nI created a new branch, which I\'m currently editing.')
+print('If you can see this, that means I successfully merged my branch with the Master')
+print('Feel free to edit this file to practice / make sure you have everything set up right!')
